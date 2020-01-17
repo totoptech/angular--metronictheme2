279cfba6a99c888c56c0cf56c621644deacf800b
webtrend55 <sinisaotmacic55@gmail.com>
2020-01-17 22:14:05 +0800

    build custom reusable text component for slaite

--- src/app/views/partials/content/widgets/slaite-label/slaite-label.component.ts
+++ src/app/views/partials/content/widgets/slaite-label/slaite-label.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+export interface Params {
+	text: string;				// Content of Label
+	size?: string;				// Size of Text (header, title, para, content)
+	color?: string;				// primary(black), gray, blue, danger
+	textClass?: string;			// class name of reusable component
+	weight?: string;			// font-weight 400, 500, 600,
+}
 
 @Component({
   selector: 'kt-slaite-label',
@@ -7,9 +15,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SlaiteLabelComponent implements OnInit {
 
-  constructor() { }
+	// Public Properties
+	@Input() param: Params;
+	public className: string;
 
-  ngOnInit() {
-  }
+	constructor() { }
+
+	ngOnInit() {
+		if (!this.param) {
+			this.param = {
+				text: "This is Slaite Reusable Label Component",
+				size: "header",
+				color: "primary",
+				textClass: "",
+				weight: "400",
+			}
+		}
+
+		// Prefix of Slaite Label Class
+		const classPrefix:string = "slate-label__";
+
+		// Slaite Label size class name
+		let sizeClass: string = "";
+		sizeClass = classPrefix + this.param.size;
+		console.log("label size:", sizeClass);
+
+		// Slaite Label color class name
+		let colorClass: string = "";
+		colorClass = classPrefix + this.param.color;
+		console.log("label color:", colorClass);
+
+		this.className = this.param.textClass;
+
+		// Slaite Label color class name
+		let fontWeightClass: string = "";
+		fontWeightClass = classPrefix + this.param.weight;
+		console.log("label font weight:", fontWeightClass);
+
+		// Combine Classes
+		this.className += sizeClass + " " + colorClass + " " + fontWeightClass;
+
+		let element = document.getElementById('slaite-label');
+		element.className = this.className;
+	}
 
 }
